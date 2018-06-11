d4cbadb7cd5d50059b307d9f5a1d4edabcdcc0c8
= <kevingray1998@live.com>
2018-06-11 20:29:18 +0100

    Changing colour of embed

--- wc.js
+++ wc.js
@@ -21,35 +21,16 @@ async function search_player(target, message){
 				if (player.PlayerFullName == target){
 
 					let player_embed = new Discord.RichEmbed();
-
 					player_embed.setTitle(`${player.PlayerDisplayName}`);
+					player_embed.setColor('0x990001');
 					player_embed.setFooter('fantasy.fifa.com', 'https://api.fifa.com/api/v1/picture/tournaments-sq-4/254645_w');
-
-					// player_embed.addField('\u200B', `Goals: ${player.goalScored}`);
-					// player_embed.addField('\u200B', `Assists: ${player.goalAssist}`);
-					// player_embed.addField('\u200B', `Position: ${player.skillDesc}`);
-
-					// player_embed.addField('\u200B', `Goals: ${player.goalScored}` + '\n' + `
-					// 						Assists: ${player.goalAssist}` + '\n' + `
-					// 						Poistion: ${player.skillDesc}` + '\n' + `
-					// 						Country: ${player.TeamName}`);
-
 					player_embed.addField('\u200B', `Goals: ${player.goalScored}` + '\n' + `Assists: ${player.goalAssist}` + '\n' + `Poistion: ${player.skillDesc}` + '\n' + `Country: ${player.TeamName}`);
-
 					player_embed.setThumbnail("https://www.fifa.com/assets/img/tournaments/common/player-placeholder--sqr.jpg");
-
-
-					console.log(player_embed);
-
 					message.channel.send(player_embed);
 				}
 			});
 		}
 	});
-}
-
-function callback(param){
-	return param;
 }
 
 client.on('ready', () => {
